c89fe18903c48c13072084fd101a20b3b0049167
LHeeremans <145541543+LHeeremans@users.noreply.github.com>
2024-11-22 01:16:22 +0100

    update4

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,14 @@
 // JavaScript Document
 console.log("hi");
+
+
+
+const menuButton = document.querySelector("nav button");
+const deNav = document.querySelector("nav");
+
+
+menuButton.onclick = toggleMenu;
+
+function toggleMenu() {
+  deNav.classList.toggle("toonMenu");
+}
